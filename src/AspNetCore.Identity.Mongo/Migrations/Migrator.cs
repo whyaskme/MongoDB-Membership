56946d6a5b737942b6db1c81cd8fd7e3a8df555f
Joseph Baranauskas <joseph.baranauskas@gmail.com>
2024-03-21 11:03:44 -0500

    Adding MongoDB membership provider project.

---
+++ src/AspNetCore.Identity.Mongo/Migrations/Migrator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using AspNetCore.Identity.Mongo.Model;
+using AspNetCore.Identity.Mongo.Mongo;
+using MongoDB.Driver;
+
+[assembly: InternalsVisibleTo("Tests")]
+
+namespace AspNetCore.Identity.Mongo.Migrations
+{
+    internal static class Migrator
+    {
+        //Starting from 4 in case we want to implement migrations for previous versions
+        public static int CurrentVersion = 6;
+
+        public static void Apply<TUser, TRole, TKey>(IMongoCollection<MigrationHistory> migrationCollection,
+            IMongoCollection<TUser> usersCollection, IMongoCollection<TRole> rolesCollection)
+            where TKey : IEquatable<TKey>
+            where TUser : MigrationMongoUser<TKey>
+            where TRole : MongoRole<TKey>
+        {
+            var version = migrationCollection
+                .Find(h => true)
+                .SortByDescending(h => h.DatabaseVersion)
+                .Project(h => h.DatabaseVersion)
+                .FirstOrDefault();
+
+            var appliedMigrations = BaseMigration.Migrations
+                .Where(m => m.Version >= version)
+                .Select(migration => migration.Apply<TUser, TRole, TKey>(usersCollection, rolesCollection))
+                .ToList();
+
+            migrationCollection.InsertMany(appliedMigrations);
+
+        }
+    }
+}
